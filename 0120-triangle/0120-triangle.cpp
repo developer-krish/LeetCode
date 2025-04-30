b9811ac74f80133c835f119ec660a5a1d8482405
Krish Gupta <165068707+developer-krish@users.noreply.github.com>
2025-04-30 23:46:47 +0530

    Time: 0 ms (100%), Space: 13.2 MB (48.91%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int>>& nums, vector<vector<int>>& dp)
-    {
-        int n = nums.size();
-        if(i == n - 1) return nums[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = nums[i][j] + func(i+1, j, nums, dp);
-        int downDia = nums[i][j] + func(i+1, j+1, nums, dp);
-        return dp[i][j] = min(down, downDia);
-    }
     int minimumTotal(vector<vector<int>>& triangle) 
     {
         int n = triangle.size();
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return func(0, 0, triangle, dp);
+        vector<vector<int>>dp(n+1, vector<int>(n, 0));
+        for(int j = 0 ; j < n ; j++)
+           dp[n-1][j] = triangle[n-1][j];
+        for(int i = n - 2 ; i >= 0 ; i--)
+        {
+            for(int j = 0 ; j < triangle[i].size() ; j++)
+            {
+                int down = triangle[i][j] + dp[i+1][j];
+                int downDia = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, downDia);
+            }
+        }
+        return dp[0][0];
     }
 };
+
+
+// ***memo***
+// class Solution {
+// public:
+//     int func(int i, int j, vector<vector<int>>& nums, vector<vector<int>>& dp)
+//     {
+//         int n = nums.size();
+//         if(i == n - 1) return nums[i][j];
+//         if(dp[i][j] != -1) return dp[i][j];
+//         int down = nums[i][j] + func(i+1, j, nums, dp);
+//         int downDia = nums[i][j] + func(i+1, j+1, nums, dp);
+//         return dp[i][j] = min(down, downDia);
+//     }
+//     int minimumTotal(vector<vector<int>>& triangle) 
+//     {
+//         int n = triangle.size();
+//         vector<vector<int>>dp(n, vector<int>(n, -1));
+//         return func(0, 0, triangle, dp);
+//     }
+// };
