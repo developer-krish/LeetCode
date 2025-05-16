348b0b18c1bbe2c9e9b7c92cda33fd1597026be4
Krish Gupta <165068707+developer-krish@users.noreply.github.com>
2025-05-16 16:48:01 +0530

    Time: 0 ms (100%), Space: 10.9 MB (42.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -14,9 +14,6 @@ public:
             if((r - l + 1) - maxFreq > k)
             {
                 freq[s[l] - 'A']--;
-                maxFreq = 0;
-                for(int i = 0 ; i < 25 ; i++) 
-                   maxFreq = max(maxFreq, freq[i]);
                 l++;
             }
             if((r - l + 1) - maxFreq <= k)
