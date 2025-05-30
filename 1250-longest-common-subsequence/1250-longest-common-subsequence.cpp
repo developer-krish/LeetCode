5a04e3555a738046c1bdae2b8516cf4789765b8f
Krish Gupta <165068707+developer-krish@users.noreply.github.com>
2025-05-31 03:49:06 +0530

    Time: 25 ms (67.56%), Space: 27.4 MB (67.31%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -5,16 +5,11 @@ public:
         int l1 = text1.size();
         int l2 = text2.size();
         vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
-        for(int i = 0 ; i <= l1 ; i++)
-           dp[i][0] = 0;
-        for(int j = 0 ; j <= l2 ; j++)
-           dp[0][j] = 0;
-        
         for(int ind1 = 1 ; ind1 <= l1 ; ind1++)
         {
             for(int ind2 = 1 ; ind2 <= l2 ; ind2++)
             {
-                if(text1[ind1 - 1] == text2[ind2 - 1])
+                if(text1[ind1 - 1] == text2[ind2 - 1]) // index shifting to right i.e i -> i-1 & j -> j-1
                     dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
                 else
                     dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1] [ind2 - 1]);
@@ -30,19 +25,19 @@ public:
 // public:
 //     int func(int ind1, int ind2, string s1, string s2, vector<vector<int>>& dp)
 //     {
-//         if(ind1 < 0 || ind2 < 0)
+//         if(ind1 == 0 || ind2 == 0)
 //            return 0;
 //         if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-//         if(s1[ind1] == s2[ind2])
+//         if(s1[ind1 - 1] == s2[ind2 - 1])
 //             return dp[ind1][ind2] = 1 + func(ind1 - 1, ind2 - 1, s1, s2, dp);
 //          return dp[ind1][ind2] = max(func(ind1 - 1, ind2, s1, s2, dp), func(ind1, ind2 - 1, s1, s2, dp));
 //     }
     
 //     int longestCommonSubsequence(string text1, string text2) 
 //     {
-        // int l1 = text1.size();
-        // int l2 = text2.size();
-        // vector<vector<int>> dp(l1, vector<int>(l2, -1));
-//         return func(l1 - 1, l2 - 1, text1, text2, dp);
+//         int l1 = text1.size();
+//         int l2 = text2.size();
+//         vector<vector<int>> dp(l1+1, vector<int>(l2+1, -1));
+//         return func(l1, l2 , text1, text2, dp); // after shifting in tabulation, it is passed as l1, l2
 //     }
 // };
