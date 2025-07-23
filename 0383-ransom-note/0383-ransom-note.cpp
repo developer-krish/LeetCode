4f458e660b200c55362c1f75fe40de1ab31ad56f
Krish Gupta <165068707+developer-krish@users.noreply.github.com>
2025-07-24 00:44:57 +0530

    Time: 11 ms (13.29%), Space: 11.9 MB (29.38%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        unordered_map<char, int> mpp;
+        for(char c : magazine)
+        {
+            mpp[c]++;
+        }
+       for (char c : ransomNote) 
+       {
+            if (mpp.find(c) == mpp.end() || mpp[c] == 0)
+                return false;
+            mpp[c]--;
+        }
+        return true;
+    }
+};
