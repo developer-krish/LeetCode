0ea8aaffca29af51b2f11b7bdeb6f6a8ecd35f6f
Krish Gupta <165068707+developer-krish@users.noreply.github.com>
2025-08-06 22:54:15 +0530

    Time: 23 ms (46.42%), Space: 43 MB (84.24%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,23 @@
+class MyCalendar {
+public:
+    MyCalendar() {}
+    vector<pair<int, int>> ranges;
+    bool book(int start, int end) 
+    {
+        for(auto const& pair : ranges)
+        {
+            int r_start = pair.first;
+            int r_end = pair.second;
+            if (start < r_end && end > r_start)
+                return false;
+        }
+        ranges.push_back(make_pair(start, end));
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
